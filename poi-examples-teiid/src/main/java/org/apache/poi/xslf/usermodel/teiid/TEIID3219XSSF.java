0d4392127bd21317b423fb9d5eaa9ef69b6e2664
ksoong <songzhiqi_1214@yahoo.com.cn>
2014-11-20 06:21:24 +0800

    add poi test sheet sample

--- poi-examples-teiid/src/main/java/org/apache/poi/xslf/usermodel/teiid/TEIID3219XSSF.java
+++ poi-examples-teiid/src/main/java/org/apache/poi/xslf/usermodel/teiid/TEIID3219XSSF.java
@@ -11,22 +11,37 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class TEIID3219XSSF {
 	
+	/*
+	 * Available xlsx files:
+	 *      3219.xlsx
+	 *      names.xlsx
+	 *      test.xlsx
+	 *      test-0.xlsx
+	 *      test-1.xlsx
+	 *      test-2.xlsx
+	 */		
 	private void test() throws Exception {
-		// 3219.xlsx
-		FileInputStream xlsFileStream = new FileInputStream("file/3219.xlsx");
+		FileInputStream xlsFileStream = new FileInputStream("file/test.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(xlsFileStream);
 		XSSFSheet sheet = workbook.getSheet("Sheet1");
 		FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
 		Iterator<Row> rowIterator = sheet.iterator();
 		
+		System.out.println("LastRowNum: " + sheet.getLastRowNum() + ", ActiveCell: " + sheet.getActiveCell());
+		
 		while(rowIterator.hasNext()) {
 			Row row = rowIterator.next();
 			for(int i = 0 ; i < row.getLastCellNum(); i ++) {
 				Cell cell = row.getCell(i, Row.RETURN_BLANK_AS_NULL);
-				System.out.print(cell + ", ");
+//				Object type = cell == null ? " " : cell.getCellType();
+				if(i == row.getLastCellNum() -1 ) {
+					System.out.println(cell);
+				} else {
+					System.out.print(cell + ", ");
+				}
+				
 			}
 						
-			System.out.println();
 		}
 		
 		
