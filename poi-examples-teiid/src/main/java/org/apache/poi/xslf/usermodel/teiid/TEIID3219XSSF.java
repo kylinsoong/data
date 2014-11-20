7d2c4d4a6788f30e924fbc7ee285f676800641b8
Kylin <kylinsoong.1214@gmail.com>
2014-11-20 16:17:40 +0800

    add excel test file

--- poi-examples-teiid/src/main/java/org/apache/poi/xslf/usermodel/teiid/TEIID3219XSSF.java
+++ poi-examples-teiid/src/main/java/org/apache/poi/xslf/usermodel/teiid/TEIID3219XSSF.java
@@ -1,6 +1,7 @@
 package org.apache.poi.xslf.usermodel.teiid;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -21,32 +22,37 @@ public class TEIID3219XSSF {
 	 *      test-2.xlsx
 	 */		
 	private void test() throws Exception {
-		FileInputStream xlsFileStream = new FileInputStream("file/test.xlsx");
+		
+		for(String file : array) {
+			XSSFSheet sheet = getSheet(file);
+			Iterator<Row> rowIterator = sheet.iterator();
+			System.out.println(file + ": LastRowNum: " + sheet.getLastRowNum() + ", ActiveCell: " + sheet.getActiveCell());
+			while(rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				for(int i = 0 ; i < row.getLastCellNum(); i ++) {
+					Cell cell = row.getCell(i, Row.RETURN_BLANK_AS_NULL);
+					if(i == row.getLastCellNum() -1 ) {
+						System.out.println(cell);
+					} else {
+						System.out.print(cell + ", ");
+					}
+				}
+			}
+			System.out.println("\n");
+		}
+
+	}
+	
+	private XSSFSheet getSheet(String file) throws Exception {
+
+		FileInputStream xlsFileStream = new FileInputStream("file/" + file);
 		XSSFWorkbook workbook = new XSSFWorkbook(xlsFileStream);
 		XSSFSheet sheet = workbook.getSheet("Sheet1");
 		FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
-		Iterator<Row> rowIterator = sheet.iterator();
-		
-		System.out.println("LastRowNum: " + sheet.getLastRowNum() + ", ActiveCell: " + sheet.getActiveCell());
-		
-		while(rowIterator.hasNext()) {
-			Row row = rowIterator.next();
-			for(int i = 0 ; i < row.getLastCellNum(); i ++) {
-				Cell cell = row.getCell(i, Row.RETURN_BLANK_AS_NULL);
-//				Object type = cell == null ? " " : cell.getCellType();
-				if(i == row.getLastCellNum() -1 ) {
-					System.out.println(cell);
-				} else {
-					System.out.print(cell + ", ");
-				}
-				
-			}
-						
-		}
-		
-		
-//		workbook.close();
+		return sheet;
 	}
+
+	String[] array = new String[]{"3219.xlsx", "test.xlsx", "test1.xlsx", "test-0.xlsx", "test-1.xlsx", "test-2.xlsx"};
 
 	public static void main(String[] args) throws Exception {
 		new TEIID3219XSSF().test();
