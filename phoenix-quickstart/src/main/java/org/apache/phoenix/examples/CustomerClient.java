a0e1cbc725d7b162b7446faaf500ece4dbf3c572
kylinsoong <kylinsoong.1214@gmail.com>
2015-01-12 16:52:41 +0800

    add jdbc with calendar

--- phoenix-quickstart/src/main/java/org/apache/phoenix/examples/CustomerClient.java
+++ phoenix-quickstart/src/main/java/org/apache/phoenix/examples/CustomerClient.java
@@ -1,6 +1,10 @@
 package org.apache.phoenix.examples;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
@@ -8,6 +12,33 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
 public class CustomerClient {
+	
+	public final static TimeZone DEFAULT_TIME_ZONE = TimeZone.getDefault();
+	
+	 static class DatabaseCalender extends ThreadLocal<Calendar> {
+	    	private String timeZone;
+	    	public DatabaseCalender(String tz) {
+	    		this.timeZone = tz;
+	    	}
+	    	@Override
+	    	protected Calendar initialValue() {
+	            if(this.timeZone != null && this.timeZone.trim().length() > 0) {
+	            	TimeZone tz = TimeZone.getTimeZone(this.timeZone);
+	                if(!DEFAULT_TIME_ZONE.hasSameRules(tz)) {
+	            		return Calendar.getInstance(tz);
+	                }
+	            }      		
+	    		return Calendar.getInstance();
+	    	}
+	    };
+	    
+	    static String databaseTimeZone = DEFAULT_TIME_ZONE.toString();
+	    
+	    static DatabaseCalender databaseCalender = new DatabaseCalender(databaseTimeZone);
+	    
+	    public static Calendar getDatabaseCalendar() {
+	    	return databaseCalender.get();
+	    }
 	
 	static {
 	      String pattern = "%d %-5p [%c] (%t) %m%n";
@@ -20,8 +51,21 @@ public class CustomerClient {
 	public static void main(String[] args) throws Exception {
 
 		Connection conn = PhoenixUtils.getPhoenixConnection("127.0.0.1:2181");
-		JDBCUtil.printTableColumn(conn, "SELECT * FROM \"Customer\"");
-		JDBCUtil.executeQuery(conn, "SELECT * FROM \"Customer\"");
+		
+		Statement stmt = conn.createStatement();
+		
+		ResultSet rs = stmt.executeQuery("SELECT g_0.\"ROW_ID\", g_0.\"q1\", g_0.\"q2\", g_0.\"q3\" FROM \"TimesTest\" AS g_0");
+		
+		
+		java.util.Calendar cal = Calendar.getInstance();
+		cal.setTimeZone(TimeZone.getDefault());
+		
+		while(rs.next()) {
+//			System.out.println(rs.getDate(2, getDatabaseCalendar() ));
+//			System.out.println(rs.getTime(3, getDatabaseCalendar()));
+			System.out.println(rs.getTimestamp(4, cal));
+		}
+		
 		JDBCUtil.close(conn);
 	}
 
---
+++ phoenix-quickstart/src/test/java/org/apache/phoenix/examples/BugReproduce.java
@@ -0,0 +1,73 @@
+package org.apache.phoenix.examples;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BugReproduce {
+	
+private static Connection conn;
+
+	static Calendar cal = Calendar.getInstance();
+	
+	
+	@BeforeClass
+	public static void init() throws Exception {
+		conn = PhoenixUtils.getPhoenixConnection("127.0.0.1:2181");
+		cal.setTimeZone(TimeZone.getDefault());
+	}
+	
+	@AfterClass
+	public static void close() throws SQLException {
+		conn.close();
+	}
+	
+	@Test
+	public void testCreateTable() throws Exception {
+		JDBCUtil.executeUpdate(conn, "CREATE TABLE IF NOT EXISTS \"TimesTest2\" (\"ROW_ID\" VARCHAR PRIMARY KEY, \"f\".\"q\" TIMESTAMP)");
+	}
+	
+	@Test
+	public void testInsert() throws SQLException {
+		Timestamp timestramp = new Timestamp(new java.util.Date().getTime());
+		String sql = "UPSERT INTO \"TimesTest2\" (\"ROW_ID\", \"q\") VALUES (?, ?)";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setString(1, "100");
+		pstmt.setTimestamp(2, timestramp, cal);
+		pstmt.addBatch();
+		
+		pstmt.executeBatch();
+		
+		JDBCUtil.close(pstmt);
+		JDBCUtil.close(conn);
+	}
+	
+	@Test
+	public void testSelect() throws Exception {
+		String sql = "SELECT * FROM \"TimesTest2\"";
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()){
+			System.out.println(rs.getTimestamp(2, cal));
+		}
+	}
+	
+	public static void main(String[ ] args) throws Exception {
+		init();
+		
+		new BugReproduce().testInsert();
+		
+		close();
+	}
+
+}
