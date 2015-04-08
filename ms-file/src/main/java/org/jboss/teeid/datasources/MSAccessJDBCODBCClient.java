46763652ee56ddb891e7824b2b26a65d0e6eb379
ksoong <songzhiqi_1214@yahoo.com.cn>
2015-04-08 11:57:05 +0800

    add jdbc access test code

--- ms-file/src/main/java/org/jboss/teeid/datasources/MSAccessJDBCODBCClient.java
+++ ms-file/src/main/java/org/jboss/teeid/datasources/MSAccessJDBCODBCClient.java
@@ -2,20 +2,26 @@ package org.jboss.teeid.datasources;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class MSAccessJDBCODBCClient {
 	
 	public static final String DNS_LESS_URL = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};Dbq=src/main/resources/ODBCTesting.mdb";
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws Exception {
 
 		dnsLessConnection();
 	}
-
-	static void dnsLessConnection() throws ClassNotFoundException, SQLException {
+	
+	static void dnsLessConnection() throws Exception {
 		Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-		Connection con = DriverManager.getConnection(DNS_LESS_URL);
+		Connection conn = DriverManager.getConnection(DNS_LESS_URL);
+		
+		JDBCUtil.executeQuery(conn, "SELECT * FROM EmpData");
+		
+		JDBCUtil.executeQuery(conn, "SELECT * FROM EmpData_test");
+		
+		JDBCUtil.close(conn);
 	}
 
 }
