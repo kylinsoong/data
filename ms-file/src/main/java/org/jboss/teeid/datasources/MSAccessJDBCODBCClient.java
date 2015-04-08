cfb75972d183a6e0bb0fe6d97c8341b4eaac839e
ksoong <songzhiqi_1214@yahoo.com.cn>
2015-04-08 12:29:25 +0800

    update access file

--- ms-file/src/main/java/org/jboss/teeid/datasources/MSAccessJDBCODBCClient.java
+++ ms-file/src/main/java/org/jboss/teeid/datasources/MSAccessJDBCODBCClient.java
@@ -6,7 +6,9 @@ import java.sql.DriverManager;
 
 public class MSAccessJDBCODBCClient {
 	
-	public static final String DNS_LESS_URL = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};Dbq=src/main/resources/ODBCTesting.mdb";
+	public static final String DNS_LESS_URL = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};DBQ=src/main/resources/ODBCTesting.mdb";
+	
+	public static final String DNS_LESS_2007_URL = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=src/main/resources/ODBCTesting.accdb";
 
 	public static void main(String[] args) throws Exception {
 
@@ -15,7 +17,7 @@ public class MSAccessJDBCODBCClient {
 	
 	static void dnsLessConnection() throws Exception {
 		Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-		Connection conn = DriverManager.getConnection(DNS_LESS_URL);
+		Connection conn = DriverManager.getConnection(DNS_LESS_2007_URL);
 		
 		JDBCUtil.executeQuery(conn, "SELECT * FROM EmpData");
 		
